45b3108ebab096f1e512336aa92f730239f47280
RainerStas <unirefsystem@gmail.com>
2018-08-25 01:49:34 +0300

    fixing error

--- src/main.cpp
+++ src/main.cpp
@@ -1261,7 +1261,7 @@ int64_t GetBlockValue(int nHeight, int64_t nFees)
         // Premine: First 10 block are 115200000 ZIJA (8% of the total coin)
         nSubsidy = 115200000 * COIN;
     } else {
-        nSubsidy = nSubsidy = (490000 - nHeight) / (490000 / 1000) * COIN;
+        nSubsidy = (490000 - nHeight) / (490000 / 1000) * COIN;
     }
 
     return nSubsidy + nFees;
