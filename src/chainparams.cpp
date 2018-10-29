74c912375ea9014514cde70f45e866df5aeaf7b5
smartinsider <smartyinsider@gmail.com>
2018-10-29 07:50:40 +0200

    update seeds and chackpionts

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -54,7 +54,7 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints =
     boost::assign::map_list_of
-      (50, uint256("0000039024ce4efabb521257f483c0017dd98726b7109a61086de3066457fdc0"))
+      (50000, uint256("0000039024ce4efabb521257f483c0017dd98726b7109a61086de3066457fdc0"))
  //   (50, uint256("00000056ed36e39376091820467610ca296f240bc660fa8c58653941e3eb113a"))
  //   (25000, uint256("70e4a6288d390e03d7f1035387739539e53a158e0b11dbed7397c2241d5af800"))
     ;
