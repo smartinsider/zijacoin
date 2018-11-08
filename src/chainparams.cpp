b77e6ff84091e6f545963684c5bbfefe35b62d64
smartinsider <smartyinsider@gmail.com>
2018-11-08 10:40:02 +0200

    zijacoin test relize

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -181,7 +181,7 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
-        fMiningRequiresPeers = false; 
+        fMiningRequiresPeers = true; 
         fAllowMinDifficultyBlocks = false;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
